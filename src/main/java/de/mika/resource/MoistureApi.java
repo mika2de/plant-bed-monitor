48142b7381de2798f7fc49b75881dd1e6b48fcf4
unknown <kaiser.briefkasten@gmail.com>
2021-07-19 19:39:12 +0200

    split resources, save sensors

---
+++ src/main/java/de/mika/resource/MoistureApi.java
@@ -0,0 +1,107 @@
+package de.mika.resource;
+
+import de.mika.database.CurrentMoistureRepository;
+import de.mika.database.DailyMoistureAvgRepository;
+import de.mika.database.HourlyMoistureAvgRepository;
+import de.mika.database.RawDataRepository;
+import de.mika.database.model.CurrentMoisture;
+import de.mika.database.model.DailyMoistureAvg;
+import de.mika.database.model.RawData;
+import de.mika.resource.model.DataPoint;
+import de.mika.resource.model.MultiChart;
+import de.mika.resource.model.SingleChart;
+import de.mika.service.LocalDateTimeService;
+import de.mika.service.MoistureService;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+public class MoistureApi {
+
+    @Inject
+    LocalDateTimeService localDateTimeService;
+
+    @Inject
+    RawDataRepository rawDataRepository;
+
+    @Inject
+    MoistureService moistureService;
+    @Inject
+    CurrentMoistureRepository currentMoistureRepository;
+
+    @Inject
+    HourlyMoistureAvgRepository hourlyMoistureAvgRepository;
+
+    @Inject
+    DailyMoistureAvgRepository dailyMoistureAvgRepository;
+
+    @GET
+    @Path("/current")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<SingleChart> getCurrent() {
+        return currentMoistureRepository.listAll()
+                .stream()
+                .sorted(Comparator.comparingInt(m -> m.getSensor().getId().intValue()))
+                .map(currentMoisture -> new SingleChart(
+                        currentMoisture.getSensor().getName(),
+                        currentMoisture.getMoisture()))
+                .collect(toList());
+    }
+
+    @GET
+    @Path("/raw")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<RawData> getRawDataForLastNMinutes(@QueryParam("minutes")  int minutes){
+        if (minutes==0)
+            return rawDataRepository.listAll();
+        return rawDataRepository.getEntriesSince(localDateTimeService.now().minusMinutes(minutes));
+    }
+
+    @GET
+    @Path("/24h")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<MultiChart> getPastHourAvgMoistures(){
+        LocalDateTime now = localDateTimeService.now();
+        List<MultiChart> multiCharts = new ArrayList<>();
+        hourlyMoistureAvgRepository.getEntriesFromToDate(now.minusHours(24).truncatedTo(ChronoUnit.HOURS), now)
+                .stream()
+                .sorted(Comparator.comparingLong(m -> m.getCreated().toEpochSecond(ZoneOffset.UTC)))
+                .forEach(hourlyMoistureAvg -> {
+                    MultiChart multiChart = multiCharts
+                            .stream()
+                            .filter(m -> m.getName() == hourlyMoistureAvg.getSensor().getName())
+                            .findFirst()
+                            .orElse(new MultiChart(hourlyMoistureAvg.getSensor().getName(), new ArrayList<>()));
+                    multiChart.getSeries().add(new SingleChart(
+                            String.valueOf(hourlyMoistureAvg.getCreated().getHour()),
+                            hourlyMoistureAvg.getAvgMoisture()));
+                    multiCharts.add(multiChart);
+                });
+        return multiCharts;
+    }
+
+    @GET
+    @Path("/daily")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<DailyMoistureAvg> getDailyAvg(){
+        return dailyMoistureAvgRepository.listAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public CurrentMoisture storeData(@Valid DataPoint dataPoint){
+        return moistureService.storeData(dataPoint.getMac(), dataPoint.getValue());
+    }
+}
--- src/main/java/de/mika/resource/SensorApi.java
+++ src/main/java/de/mika/resource/SensorApi.java
@@ -22,39 +22,41 @@ import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
-@Path("/moisture")
+@Path("/sensors")
 public class SensorApi {
-
-    @Inject
-    LocalDateTimeService localDateTimeService;
-
-    @Inject
-    RawDataRepository rawDataRepository;
-
-    @Inject
-    MoistureService moistureService;
 
     @Inject
     SensorRepository sensorRepository;
 
-    @Inject
-    CurrentMoistureRepository currentMoistureRepository;
-
-    @Inject
-    HourlyMoistureAvgRepository hourlyMoistureAvgRepository;
-
-    @Inject
-    DailyMoistureAvgRepository dailyMoistureAvgRepository;
-
     @GET
-    @Path("/sensors")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Sensor> getSensors() {
-        return sensorRepository.listAll();
+        return sensorRepository.listAll().
+                stream()
+                .sorted(Comparator.comparingInt(m -> m.getId().intValue()))
+                .collect(toList());
     }
 
     @POST
-    @Path("/sensors/{sensorId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public List<Sensor> storeSensors(List<Sensor> sensorList) {
+        List<Sensor> updatedSensorList = new ArrayList<>();
+        List<Sensor> databaseSensorList = sensorRepository.listAll().
+                stream()
+                .sorted(Comparator.comparingInt(m -> m.getId().intValue()))
+                .collect(toList());
+        sensorList.forEach(sensor -> {
+            Sensor updatedSensor = databaseSensorList.stream().filter(dbSensor -> dbSensor.getId() == sensor.getId()).findFirst().get();
+            updatedSensor.setName(sensor.getName());
+            sensorRepository.persist(updatedSensor);
+            updatedSensorList.add(updatedSensor);
+        });
+        return updatedSensorList;
+    }
+
+    @POST
+    @Path("/{sensorId}")
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
     public Sensor renameSensor(@PathParam(value = "sensorId") int sensorId, @NotNull String name){
@@ -64,67 +66,4 @@ public class SensorApi {
         return sensor;
     }
 
-
-    @GET
-    @Path("/current")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<SingleChart> getCurrent() {
-        return currentMoistureRepository.listAll()
-                .stream()
-                .sorted(Comparator.comparingInt(m -> m.getSensor().getId().intValue()))
-                .map(currentMoisture -> new SingleChart(
-                        currentMoisture.getSensor().getName(),
-                        currentMoisture.getMoisture()))
-                .collect(toList());
-    }
-
-    @GET
-    @Path("/raw")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<RawData> getRawDataForLastNMinutes(@QueryParam("minutes")  int minutes){
-        if (minutes==0)
-            return rawDataRepository.listAll();
-        return rawDataRepository.getEntriesSince(localDateTimeService.now().minusMinutes(minutes));
-    }
-
-    @GET
-    @Path("/24h")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<MultiChart> getPastHourAvgMoistures(){
-        LocalDateTime now = localDateTimeService.now();
-        List<MultiChart> multiCharts = new ArrayList<>();
-        hourlyMoistureAvgRepository.getEntriesFromToDate(now.minusHours(24).truncatedTo(ChronoUnit.HOURS), now)
-                .stream()
-                .sorted(Comparator.comparingLong(m -> m.getCreated().toEpochSecond(ZoneOffset.UTC)))
-                .forEach(hourlyMoistureAvg -> {
-                    MultiChart multiChart = multiCharts
-                            .stream()
-                            .filter(m -> m.getName() == hourlyMoistureAvg.getSensor().getName())
-                            .findFirst()
-                            .orElse(new MultiChart(hourlyMoistureAvg.getSensor().getName(), new ArrayList<>()));
-                    multiChart.getSeries().add(new SingleChart(
-                            String.valueOf(hourlyMoistureAvg.getCreated().getHour()),
-                            hourlyMoistureAvg.getAvgMoisture()));
-                    multiCharts.add(multiChart);
-                });
-        return multiCharts;
-//                .sorted(Comparator.comparingInt(m -> m.getSensor().getId().intValue()))
-//                .
-//                .map(hourlyMoistureAvg -> )
-//                .collect(Collectors.groupingBy(o -> ));
-    }
-
-    @GET
-    @Path("/daily")
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<DailyMoistureAvg> getDailyAvg(){
-        return dailyMoistureAvgRepository.listAll();
-    }
-
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
-    public CurrentMoisture storeData(@Valid DataPoint dataPoint){
-        return moistureService.storeData(dataPoint.getMac(), dataPoint.getValue());
-    }
 }
